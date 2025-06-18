8ed3eb5efe49bc87997b6ae20c06b8ea6ba600e0
yurani <thelara270@gmail.com>
2025-06-17 22:01:22 -0500

    hshshhhh

--- Assets/assetsD/PanelSwitcher.cs
+++ Assets/assetsD/PanelSwitcher.cs
@@ -4,10 +4,20 @@ public class PanelSwitcher : MonoBehaviour
 {
     public GameObject currentPanel;
     public GameObject nextPanel;
+    public GameObject boton;
+    public GameObject animacion;
 
     public void SwitchPanel()
     {
         currentPanel.SetActive(false);
         nextPanel.SetActive(true);
+    }
+
+    public void SkipPanel()
+    {
+        currentPanel.SetActive(true);
+        nextPanel.SetActive(false);
+        boton.SetActive(false);
+        animacion.SetActive(false);
     }
 }
